4b546abb89131d886eeb437cdd54ca68d05dc7b9
Ashok <ashokkumarmula422@gmail.com>
2020-01-07 10:00:28 +0530

    My first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+Web:gunicorn MBF.wsgi
---
+++ Weapons/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+
+# Create your views here.
+def Welcome(request):
+    return render(request, 'weapons/Welcome.html')
+def Home(request):
+    return render(request, 'weapons/base.html')
+
+def logoutpage(request):
+    return render(request,'Weapons/logout.html')
+def signup(request):
+    form = UserCreationForm(request.POST)
+    if request.method=='POST':
+
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            form= UserCreationForm()
+    return render(request,'Weapons/registration.html', {'form':form})
+@login_required()
+def Link(request):
+    return render(request,'registration/login.html')
+
+
+
